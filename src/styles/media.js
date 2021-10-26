7e8c5824324dbb7258df1f2a9f686e0d765534d4
Евгений Заверзин <lesmons@192.168.0.10>
2021-10-26 16:48:27 +0700

    fix media

--- src/styles/media.js
+++ src/styles/media.js
@@ -1,4 +1,4 @@
 export const media = {
-  desktop: "@media only screen and (min-width: 1024px) and (max-width: 1920px)",
+  desktop: "@media only screen and (min-width: 1024px) and (max-width: 2560px)",
   mobile: "@media only screen and (min-width: 0px) and (max-width: 1024px)",
 };
